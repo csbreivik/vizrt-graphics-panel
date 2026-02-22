4b5227b7cc908d12552b5fb38f597840efe0026e
csbreivik <csbreivik@outlook.com>
2026-02-22 13:42:45 +0100

    index.ts - createWebHistory > createWebHashHistory

--- src/app/router/index.ts
+++ src/app/router/index.ts
@@ -1,9 +1,9 @@
-import { createRouter, createWebHistory } from "vue-router";
+import { createRouter, createWebHashHistory } from "vue-router";
 import ControlPanel from "../../pages/ControlPanel.vue";
 import Output from "../../pages/Output.vue";
 
 export default createRouter({
-  history: createWebHistory(),
+  history: createWebHashHistory(),
   routes: [
     { path: "/", redirect: "/control" },
     { path: "/control", component: ControlPanel },
